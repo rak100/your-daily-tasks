2d38d335d28295b0f82a76731e37f88f9ef72f1b
rak10 <ahmadus10@icloud.com>
2023-04-01 04:19:47 +0500

    added header component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 
+// COMPONENTS import
+import Header from './components/Header';
+
 function App() {
   return (
     <div>
-      Hello
+      <Header />
     </div>
   );
 }
---
+++ client/src/components/header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <Header>
+      <h1>React App to Keep Track of Your Daily Tasks</h1>
+    </Header>
+  );
+};
+
+export default Header;
