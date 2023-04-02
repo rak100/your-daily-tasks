d0916263a56e96bbf7c763eeba6f33ae0b7a879a
rak10 <ahmadus10@icloud.com>
2023-04-03 02:37:41 +0500

    added todo list

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 
 // COMPONENTS import
 import Header from "./components/Header";
+import TodoForm from "./components/TodoForm";
 
 function App() {
   return(
     <div>
       <Header />
+      <TodoForm />
     </div>
   )
 }
---
+++ client/src/components/TodoForm.jsx
@@ -0,0 +1,8 @@
+const TodoForm = () => {
+  return (
+    <form>
+      <input className="task_input" placeholder="Enter your task here" />
+    </form>
+  );
+};
+export default TodoForm;
