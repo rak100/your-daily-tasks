193231b88eeaea03e3e4f897f078bf7c9caa2179
rak10 <ahmadus10@icloud.com>
2023-04-03 03:19:00 +0500

    added todo list functionality

--- client/src/components/TodoForm.jsx
+++ client/src/components/TodoForm.jsx
@@ -1,7 +1,22 @@
-const TodoForm = () => {
+import { useState } from "react";
+
+
+const TodoForm = () => { 
+
+
+const [text, setText] = useState("");
+
+const onFormSubmit = () =>{
+
+}
+   const onInputChange = (e) =>{
+    setText(e.target.value);
+    }
   return (
-    <form>
-      <input className="task_input" placeholder="Enter your task here" />
+    <form onSubmit={onFormSubmit}>
+      <input className="task_input"
+        placeholder="Enter your task here"
+        onChange={onInputChange} />
     </form>
   );
 };
